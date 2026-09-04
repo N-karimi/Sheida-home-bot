d08ef324bbe124a2fd8f20b5cbdc60b398d226a4
Nadia <nadiak13845@gmail.com>
2026-09-04 04:06:13 +0330

    17 tests database

--- config.py
+++ config.py
@@ -7,4 +7,7 @@ load_dotenv()
 database_name = os.environ.get('database_name')
 database_config = {'user': os.environ.get('database_user'), 'password': os.environ.get('database_password'), 'host': os.environ.get('database_host')}
 
+print('database name:', database_name)
+print('host:', database_config['host'])
+print('d user:', database_config['user'])
 #print(database_config)
